7001ba5b253e838abc3bc64c3139d6f39ce47ab3
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-11 22:01:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,6 +46,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(!cardNumber){
-    return ", we don't have a credit card on file for you.";
+    return "Sorry, we don't have a credit card on file for you.";
   }
 }
